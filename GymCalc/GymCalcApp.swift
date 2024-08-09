f81d687dbeba58bb7c7a2b0914d7e57edcaf5e07
Srinivas Ravi <srinivas.ravi.official@gmail.com>
2024-08-09 13:52:03 -0700

    Initial Commit

---
+++ GymCalc/GymCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymCalcApp.swift
+//  GymCalc
+//
+//  Created by Srinivas Ravi on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GymCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
